a78d0b5bb56d5997a1d7c7d2b2b2f1c1d9a4db67
Chelsea Trotter <chelsea.xhu@gmail.com>
2020-11-16 08:39:40 -0600

    removing dead code

--- src/data_io.jl
+++ src/data_io.jl
@@ -5,13 +5,6 @@ function get_geno_data(file, datatype)
     return geno_prob[:,1:2:end]
 end
 
-# function get_pheno_data(file, datatype)
-
-#     #first column is individual ID such as : BXD1
-#     pheno = readdlm(file, ','; skipstart=1)
-#     return convert(Array{datatype,2}, pheno)
-# end
-
 function try_string2num(num)
     return tryparse(Float64,num) != nothing
 end
