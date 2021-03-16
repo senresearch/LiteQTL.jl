c7470d7e7355ae92b0886e0260c63d1488696fbc
Chelsea Trotter <xiaoqihu@tux03.uthsc.edu>
2021-03-16 16:36:41 -0500

    standardizing matrix now can be done on GPU

--- src/gpu.jl
+++ src/gpu.jl
@@ -4,7 +4,7 @@ end
 
 function get_pheno_block_size(n::Int, m::Int, p::Int, datatype::DataType)
     total_data_size = (n*m + n*p + m*p) * sizeof(datatype) # get the number of bytes in total
-    gpu_mem = CUDA.available_memory() * 0.9 # can not use all of gpu memory, need to leave some for intermediate result.
+    gpu_mem = CUDA.available_memory() * 0.89 # can not use all of gpu memory, need to leave some for intermediate result.
     #if m is too big for gpu memory, I need to seperate m into several blocks to process
     block_size = Int(ceil((gpu_mem - (n*p))/((n+p) * sizeof(datatype))))
     num_block = Int(ceil(m/block_size))
@@ -29,9 +29,11 @@ function gpurun(Y::Array{<:Real,2}, G::Array{<:Real,2},n)
     (num_block, block_size) = get_pheno_block_size(n,m,p, typeof(Y[1,1]))
     println("seperated into $num_block blocks, containing $block_size individual per block. ")
 
-    g_std = get_standardized_matrix(G); # 3.39 seconds
+    d_g = CuArray(G)
+    d_g_std = get_standardized_matrix_gpu(d_g); # 3.39 seconds
+    # CUDA.unsafe_free!(d_g)
     lod = convert(Array{typeof(Y[1,1]), 2},zeros(0,2))
-    d_g = CuArray(g_std); #0.3 seconds
+    # d_g = CuArray(g_std); #0.3 seconds
     for i = 1:num_block
         # i = 1
         begining = block_size * (i-1) +1
@@ -42,11 +44,15 @@ function gpurun(Y::Array{<:Real,2}, G::Array{<:Real,2},n)
         # println("processing $begining to $ending...")
 
         y_block = Y[:, begining : ending]
-        y_std = get_standardized_matrix(y_block);
+        d_y = CuArray(y_block)
+        d_y_std = get_standardized_matrix_gpu(d_y);
+        # CUDA.unsafe_free!(d_y)
 
-        d_y = CuArray(y_std);
-        d_r = calculate_r(d_y,d_g);
+        # d_y = CuArray(y_std);
+        d_r = calculate_r(d_y_std,d_g_std);
         actual_block_size = ending - begining + 1 #it is only different from block size at the last loop since we are calculating the left over block not a whole block.
+        # CUDA.unsafe_free!(d_y_std)
+        # CUDA.unsafe_free!(d_g_std)
         gpu_square_lod(d_r,n,actual_block_size,p)
 
         lod = vcat(lod, collect(d_r[:, 1:2]))
@@ -74,7 +80,7 @@ function gpurun(Y::Array{<:Real,2}, G::Array{<:Real,2}, X::Array{<:Real,2}, n)
     # Calculate tilda on GPU
     d_g_tilda = d_g .- d_g_hat 
     # Standardizing matrix on GPU
-    d_g_std = get_standardized_matrix(d_g_tilda)
+    d_g_std = get_standardized_matrix_gpu(d_g_tilda)
 
     for i = 1:num_block
         # i = 1
@@ -92,7 +98,7 @@ function gpurun(Y::Array{<:Real,2}, G::Array{<:Real,2}, X::Array{<:Real,2}, n)
         d_y_hat = CUDA.CUBLAS.gemm('N', 'N', d_px, d_y)
         # Calculate y tilda on GPU
         d_y_tilda = d_y .- d_y_hat
-        d_y_std = get_standardized_matrix(d_y_tilda)
+        d_y_std = get_standardized_matrix_gpu(d_y_tilda)
 
 
         # y_std = get_standardized_matrix(y_block);
