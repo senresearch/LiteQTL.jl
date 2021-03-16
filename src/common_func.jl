f248c9baa2f7beb3654f5c61e8b75026fbdbdd8b
Chelsea Trotter <xiaoqihu@tux03.uthsc.edu>
2021-03-16 16:34:42 -0500

    fixed error, that std calculation does not agree with preious result

--- src/common_func.jl
+++ src/common_func.jl
@@ -1,26 +1,28 @@
-function get_standardized_matrix(m::AbstractArray{<:Real,2})
-    return m = (m .- mean(m)) ./ std(m)
+function std_mat(m)
+    return (m .- mean(m, dims=1)) ./ std(m, corrected=false, dims=1) 
 end
 
-# function get_standardized_matrix(m::AbstractArray{<:Real,2})
-#     Threads.@threads for col in 1:size(m)[2]
-#         summ = 0.0f0
-#         rows = size(m)[1]
-#         for row in 1:rows
-#             summ += m[row, col]
-#         end
-#         mean = summ/rows
-#         sums = 0.0f0
-#         for row in 1:rows
-#             sums += (m[row,col] - mean)^2
-#         end
-#         std = sqrt(sums/rows)
-#         for row in 1:rows
-#             m[row,col] = (m[row,col]-mean)/std
-#         end
-#     end
-#     return m
-# end
+function get_standardized_matrix(m::AbstractArray{<:Real,2})
+    Threads.@threads for col in 1:size(m)[2]
+        summ = 0.0f0
+        rows = size(m)[1]
+        for row in 1:rows
+            summ += m[row, col]
+        end
+        mean = summ/rows
+        sums = 0.0f0
+        for row in 1:rows
+            sums += (m[row,col] - mean)^2
+        end
+        std = sqrt(sums/rows)
+        for row in 1:rows
+            m[row,col] = (m[row,col]-mean)/std
+        end
+        println(mean)
+        println(std)
+    end
+    return m
+end
 
 # function new_std_matrix(m::AbstractArray{<:Real,2})
 #     rownum = size(m)[1]
